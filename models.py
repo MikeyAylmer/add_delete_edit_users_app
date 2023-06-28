4fd9986bd1a99082988a3efc0213fb8614ecc0b8
Michael Aylmer <michaelaylmer@Michaels-MacBook-Air.local>
2023-06-28 09:45:24 -0400

    made a post page

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text, select
+import datetime
 
 db = SQLAlchemy()
 
@@ -25,3 +26,17 @@ class User(db.Model):
     image_url = db.Column(db.String,
                           nullable=False,
                           unique=True)
+
+class Post(db.Model):
+    """Blog post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
